34cc43954167149f410fe26e48edf26c37d8b4fc
bdintu <j@bdintu.com>
2019-09-14 21:14:30 +0700

    Add select mask

--- main.cc
+++ main.cc
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
-#define TABLE_SIZE 5
+#define MASK_NUM 5
+#define TABLE_SIZE 5*5
 #define PROP_OF_EVENT 20
 
 using namespace std;
 
 struct Node{
-	int table[TABLE_SIZE*TABLE_SIZE];
+	int table[TABLE_SIZE];
 	Node *next[PROP_OF_EVENT];
 };
 
@@ -18,58 +21,121 @@ public:
 	~FlowFree();
 
     void readFile(const char* path);
+    void selectMask();
 
 private:
-	void create_tree();
-	void destroy_tree();
+	void createTree();
+	void destroyTree();
 
 	Node *root;
 };
 
 
 FlowFree::FlowFree(){
-    create_tree();
+    createTree();
 }
 
 FlowFree::~FlowFree(){
-	destroy_tree();
+	destroyTree();
 }
 
-void FlowFree::create_tree(){
+void FlowFree::createTree(){
 	root = NULL;
 }
 
-void FlowFree::destroy_tree(){
+void FlowFree::destroyTree(){
 }
 
 
 void FlowFree::readFile(const char* path) {
     Node *node = new Node;
-    for(int i=0; i<PROP_OF_EVENT; i++) {
+    for(int i=0; i<PROP_OF_EVENT; i++)
         node->next[i] = NULL;
-    }
 
     ifstream file(path) ;
     if (file) {
-        int i = 0;
-        while(file) {
+        clog << endl << "# read file " << path << endl;
+        for (int i=0; i<TABLE_SIZE; i++) {
             file >> node->table[i];
-            i++;
+            clog << "index: " << i << " mask: " << node->table[i] << endl;
         }
     } else {
-        std::cerr << "failed to open file " << path << endl;
+        cerr << "failed to open file " << path << endl;
     }
 
     root = node;
 }
 
+bool canwalkSort(const vector<int>& v1, const vector<int>& v2) { 
+    return v1[2] > v2[2]; 
+} 
+
+void FlowFree::selectMask() {
+    vector<vector<int>> canwalk(2*MASK_NUM, vector<int>(3, 0));
+    auto it = canwalk.begin();
+
+    clog << endl << "# select mask" << endl;
+    for (int i=0; i<TABLE_SIZE; i++) {
+        if (root->table[i] != 0) {
+            (*it) = {i, root->table[i], 0};
+
+            if (i%MASK_NUM !=0 && root->table[i-1] == 0)
+                (*it)[2]++;
+
+            if (i%MASK_NUM !=MASK_NUM-1 && root->table[i+1] == 0)
+                (*it)[2]++;
+
+            if (i/MASK_NUM !=0 && root->table[i-5] == 0)
+                (*it)[2]++;
+
+            if (i/MASK_NUM !=MASK_NUM-1 && root->table[i+5] == 0)
+                (*it)[2]++;
+
+            clog << "index: " << (*it)[0]
+                << ", mask: " << (*it)[1]
+                << ", canwalk: " << (*it)[2]
+                << endl;
+
+            advance(it, 1);
+        }
+    }
+
+    sort(canwalk.begin(), canwalk.end(), canwalkSort);
+
+    clog << endl << "# select mask after sorted" << endl;
+    for (auto& it: canwalk)
+        clog << "index: " << it[0]
+            << ", mask: " << it[1]
+            << ", canwalk: " << it[2]
+            << endl;
+
+
+
+    vector<int> mask_duplicate(5);
+
+    for (auto& it: canwalk) {
+        if (find(mask_duplicate.begin(), mask_duplicate.end(), it[1])  == mask_duplicate.end()) {
+            mask_duplicate.push_back(it[1]);
+
+            root->table[it[0]] = it[1] + MASK_NUM;
+        }
+    }
+
+    clog << endl << "# table after select mask" << endl;
+    for (int i=0; i<TABLE_SIZE; i++)
+        clog << "index: " << i << " mask: " << root->table[i] << endl;
+}
+
+
 int main(int argc, char** argv) {
     if (!argc) {
+        cerr << "no find arg";
         return 0;
     }
 
     FlowFree *flowfree = new FlowFree();
     flowfree->readFile(argv[1]);
+    flowfree->selectMask();
 
     return 0;
 }
